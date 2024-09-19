03e73acbae74dffdda5e7da41dd03b4159e26960
AntonisZks <antoniszks2003@gmail.com>
2024-09-19 13:31:51 +0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import app_logo from "../../assets/images/medium_logo.png";
+import profile_image from "../../assets/images/profile_image.jpg";
+import NavigationBar from "../NavigationBar/NavigationBar";
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <div className={styles.header_container}>
+        <img
+          className={styles.application_logo}
+          src={app_logo}
+          alt="app_logo"
+        />
+        <NavigationBar />
+        <div className={styles.user_online_container}>
+          <img src={profile_image} alt="profile_picture" />
+        </div>
+        <div className={styles.expand_menu_container}>
+          <button>
+            <i className="fa-solid fa-bars"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,52 @@
+import styles from "./NavigationBar.module.css";
+
+export default function NavigationBar() {
+  return (
+    <nav className={styles.navigation_bar}>
+      <ul>
+        <li>
+          <a href="#">
+            <i className="fa-solid fa-house"></i>
+            <p>Home</p>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fa-solid fa-user-group"></i>
+            <p>Network</p>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fa-solid fa-magnifying-glass"></i>
+            <p>Jobs</p>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fa-solid fa-comments"></i>
+            <p>Discussions</p>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fa-solid fa-bell"></i>
+            <p>Notifications</p>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fa-solid fa-user"></i>
+            <p>Personal Data</p>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fa-solid fa-gear"></i>
+            <p>Settings</p>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
